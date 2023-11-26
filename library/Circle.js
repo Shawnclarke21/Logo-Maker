45f8b8011d6b0828b489c7296184a4cbd0cc6950
unknown <srclarke777@gmail.com>
2023-11-25 17:38:23 -0700

    made it so that it appears with an extentsion so i can view my svg also changed some of the measurments so that the trinagle square and circles look correct and the text actually appears inside of the shapes also fixed some of the formatting

--- library/Circle.js
+++ library/Circle.js
@@ -3,8 +3,8 @@ const Shape = require("./shapes");
 class Circle extends Shape {
     render() {
         return `<svg height="500" width="500">
-            <circle cx="250" cy="250" r="250" fill="${this.color}"/>
-            <text fill="${this.textColor}" font-size="75" x="172" y="250">${this.text}</text>
+            <circle cx="250" cy="250" r="200" fill="${this.color}"/>
+            <text fill="${this.textColor}" font-size="80" x="172" y="250">${this.text}</text>
         </svg>`;
     }
 }
--- library/Triangle.js
+++ library/Triangle.js
@@ -2,9 +2,9 @@ const Shape = require("./shapes");
 
 class Triangle extends Shape {
     render() {
-        return `<svg height="210" width="700">
-            <polygon points="200,50 300,300 100,300" fill="${this.color}"/>
-            <text fill="${this.textColor}" font-size="75" x="172" y="250">${this.text}</text>
+        return `<svg height="500" width="500">
+            <polygon points="140,0 300,200 0,200" fill="${this.color}"/>
+            <text fill="${this.textColor}" font-size="75" x="85" y="130">${this.text}</text>
         </svg>`;
     }
 }
